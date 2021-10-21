99f56019cef70db9cfd9bc858a5dd9440849d8e6
mschulte <mschulte@eogresources.com>
2021-10-21 16:44:19 -0500

    initialize with schema

---
+++ cmd/ts-label/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+	"github.com/maxsei/ts-label/src/graph"
+	"github.com/maxsei/ts-label/src/graph/generated"
+)
+
+const defaultPort = "8080"
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+	// Setting up Gin
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/maxsei/ts-label
+
+go 1.16
+
+require (
+	github.com/99designs/gqlgen v0.14.0
+	github.com/gin-gonic/gin v1.7.4
+	github.com/vektah/gqlparser/v2 v2.2.0
+)
